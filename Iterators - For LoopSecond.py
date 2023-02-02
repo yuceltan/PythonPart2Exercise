06c67f1482188575fd6918c58b01ddf406b62998
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-02-03 02:07:45 +0300

    third question

---
+++ Iterators - For LoopSecond.py
@@ -0,0 +1,5 @@
+todo = ["exercise1", "exercise2", "exercise3", "coffee break", "exercise4", "exercise5", "exercise6"]
+for x in todo:
+    if x.upper() == "COFFEE BREAK":
+        print(x)
+        break
