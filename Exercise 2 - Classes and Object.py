8f3948eff4f76f7e72198e939d68c566ca62dbdb
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-02-03 18:31:57 +0300

    Exercise 2 - Classes and Object

---
+++ Exercise 2 - Classes and Object.py
@@ -0,0 +1,19 @@
+class Animal:
+    def __init__(self,legs_count):
+        print("Animal object was created")
+        self.number_of_legs = legs_count
+
+    def runs(self):
+        print("Running started")
+
+    def count_legs(self):
+        print(f"It has {self.number_of_legs}")
+
+    def return_legs(self):
+        return print("It has",self.number_of_legs)
+
+animal = Animal(4)
+
+animal.count_legs()
+animal.return_legs()
+print(animal)
