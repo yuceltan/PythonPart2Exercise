6a0605b84d6e76b62f963e02d2162e32e01e053f
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-02-06 22:11:41 +0300

    Error is fixed!

--- Exercise 7 - Functions.py
+++ Exercise 7 - Functions.py
@@ -1,7 +1,10 @@
 my_list= [*range(5)]
 x=lambda func:pow(func,2)
+
 for i in range(len(my_list)):
-        print(x(my_list[i]))
+        if my_list[i] % 2 == 0:
+                print(x(my_list[i]))
+
 
 
 
